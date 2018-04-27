ca2fb3b1f437cbe101bab00185ff65e5e8680c1e
Lucas Paganini de Moura <lucaspaganinidm@gmail.com>
2018-04-27 00:07:19 -0300

    add logic signature

--- src/scripts/checkInfinityWarTickets.ts
+++ src/scripts/checkInfinityWarTickets.ts
@@ -1,4 +1,14 @@
 import * as puppeteer from 'puppeteer'
+
+const closeBannerIfNecessary = () => {
+  const buttonToCloseBanner: HTMLButtonElement = document.querySelector(
+    '#pre-site > header > div > div.col-xs-8.col-md-10.text-xs-right > button',
+  )
+}
+
+const getAvailableDates = (): string[] => {
+  return []
+}
 ;(async () => {
   const browser = await puppeteer.launch({
     headless: process.env.NODE_ENV !== 'development',
@@ -9,4 +19,10 @@ import * as puppeteer from 'puppeteer'
     'https://www.ingresso.com/sao-paulo/home/cinemas/uci-analia-franco',
     { waitUntil: 'networkidle0' },
   )
+
+  const isSellingTickets: boolean = await page.evaluate(() => {
+    closeBannerIfNecessary()
+    const dates = getAvailableDates()
+    return dates.includes('05/05')
+  })
 })()
