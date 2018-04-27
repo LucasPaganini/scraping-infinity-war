4f1330584f6bb9f238049adf2a677f851fe11f19
Lucas Paganini de Moura <lucaspaganinidm@gmail.com>
2018-04-27 00:14:37 -0300

    improve logic for closeBannerIfNecessary

--- src/scripts/checkInfinityWarTickets.ts
+++ src/scripts/checkInfinityWarTickets.ts
@@ -1,9 +1,17 @@
 import * as puppeteer from 'puppeteer'
 
-const closeBannerIfNecessary = () => {
+const closeBannerIfNecessary = async () => {
   const buttonToCloseBanner: HTMLButtonElement = document.querySelector(
     '#pre-site > header > div > div.col-xs-8.col-md-10.text-xs-right > button',
   )
+  return new Promise(resolve => {
+    // If there's no banner, resolve
+    if (!buttonToCloseBanner) resolve()
+
+    // If there's a banner, close it and resolve after 2 seconds
+    buttonToCloseBanner.click()
+    setTimeout(resolve, 2000)
+  })
 }
 
 const getAvailableDates = (): string[] => {
@@ -20,8 +28,8 @@ const getAvailableDates = (): string[] => {
     { waitUntil: 'networkidle0' },
   )
 
-  const isSellingTickets: boolean = await page.evaluate(() => {
-    closeBannerIfNecessary()
+  const isSellingTickets: boolean = await page.evaluate(async () => {
+    await closeBannerIfNecessary()
     const dates = getAvailableDates()
     return dates.includes('05/05')
   })
